5e0ea9bb704695be6de0ede54ed35fb1201b50d9
hhl41446 <653569384@qq.com>
2023-09-21 10:54:29 +0800

    update

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hello")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!") // 返回一个简单的响应
+	})
+
+	fmt.Println("Listening on port 8080...")
+	http.ListenAndServe(":8080", nil) //监听端口并处理请求
 }
